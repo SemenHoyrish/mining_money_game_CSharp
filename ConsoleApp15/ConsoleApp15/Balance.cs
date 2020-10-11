beecaa60994c9cbbd423e05e3a731e4650d5d960
SemenHoyrish <63116519+SemenHoyrish@users.noreply.github.com>
2020-10-11 19:28:49 +0500

    Add files via upload

---
+++ ConsoleApp15/ConsoleApp15/Balance.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+    class Balance
+    {
+        private int _money = 0;
+
+        public int getMoneyCount()
+        {
+            return _money;
+        }
+
+        public void addMoney(int number)
+        {
+            _money += number;
+        }
+
+        public bool buySomething(int cost)
+        {
+            if (_money >= cost)
+            {
+                _money -= cost;
+                return true;
+            }
+            return false;
+        }
+    }
+}
